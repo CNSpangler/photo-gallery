eee5844275972164827556cf833c46fef7cab964
Chelsea Spangler <chelseanspangler@gmail.com>
2020-02-12 16:36:53 -0800

    unicorn filter works

--- src/App.js
+++ src/App.js
@@ -1,19 +1,50 @@
-// App.js - top level component. App.js imports the horned creatures data and passes it to ImageList.js
-// The App component should introduce the image list data, passing as a prop to ImageList.
-
-import React from 'react';
+import React, { Component } from 'react';
+import request from 'superagent';
 import './App.css';
 import Header from './Header.js';
 import ImageList from './ImageList.js';
 import images from './images.js';
+import { render } from '@testing-library/react';
 
-function App() {
-  return (
-    <div>
-      <Header />
-      <ImageList imageData = {images} />
-    </div>
-  );
+export default class App extends Component {
+  state = { selected: null };
+
+  render() {
+    const handleChange = e => {
+      this.setState({ selected: e.target.value });
+    };
+    
+    const keywordFilter = images.filter(image => {
+      if(!this.state.selected) return true;
+      return image.keyword === this.state.selected;
+    });
+    
+    // const hornsFilter = images.filter(image => {
+    //   if(!this.state.selected) return true;
+    //   const state = parseInt(this.state.selected)
+    //   return image.horns === state;
+    // });
+    
+    return (
+      <div>
+        <Header />
+        {JSON.stringify(this.state.data)}
+        <select className="unicornFilter" onChange={handleChange}>
+          <option value='' defaultValue>All</option>
+          <option value='narwhal'>Narwhal</option>
+          <option value='rhino'>Rhino</option>
+          <option value='unicorn'>Unicorn</option>
+          <option value='unilego'>Unilego</option>
+          <option value='triceratops'>Triceratops</option>
+          <option value='markhor'>Markhor</option>
+          <option value='mouflon'>Mouflon</option>
+          <option value='addax'>Addax</option>
+          <option value='chameleon'>Chameleon</option>
+          <option value='lizard'>Lizard</option>
+          <option value='dragon'>Dragon</option>
+        </select>
+        <ImageList imageData = {keywordFilter} />
+      </div>
+    );
+  }
 }
-
-export default App;
--- src/ImageItem.js
+++ src/ImageItem.js
@@ -3,12 +3,13 @@ import React, { Component } from 'react';
 export default class ImageItem extends Component {
     render() {
         return(
-            <li>
-                <h3>{this.props.imageTitle}</h3>
-                <img src={this.props.imageSource} alt={this.props.imageDescription} />
-                <h4>This creature has {this.props.imageHorns} horn(s)! 
-                {this.props.imageDescription}</h4>
-            </li>
+            <div id="li-div">
+                <li>
+                    <h3>{this.props.imageTitle}</h3>
+                    <img src={this.props.imageSource} alt={this.props.imageDescription} />
+                    <h4>This creature has {this.props.imageHorns} horn(s)!</h4>
+                </li>
+            </div>
         )
     }
 };
